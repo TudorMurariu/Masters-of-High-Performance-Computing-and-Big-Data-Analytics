93f5bf01d3cd339eb9bf5f60569210d2722f0262
TudorMurariu <tudorcristianmurariu@yahoo.com>
2024-12-06 16:58:32 +0200

    some new stuff

---
+++ an1/Sem1/Advanced Methods of Data Analysis/R1/KNN_implementation.py
@@ -0,0 +1,12 @@
+from cmath import sqrt
+
+
+# calculate the Euclidean distance between two vectors
+def euclidean_distance(row1, row2):
+	distance = 0.0
+	for i in range(len(row1)-1):
+		distance += (row1[i] - row2[i])**2
+	return sqrt(distance)
+
+
+
